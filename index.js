a01d140e781dcb5056f8dc2a817404b54390bdac
Zabdiel Ramos <zabdiel.ramos01@gmail.com>
2018-02-26 03:49:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target')
 }
-function 
+function increaseRankBy(n) {
+  
+}
