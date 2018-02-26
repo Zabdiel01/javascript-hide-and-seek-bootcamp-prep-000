dded4cf51b8ab4baa58eb41bbbe7740331391da9
Zabdiel Ramos <zabdiel.ramos01@gmail.com>
2018-02-26 04:23:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target')
 }
 function increaseRankBy(n) {
-  var item = document.getElementById('app').querySelectorAll('main ul.ranked-list li').innerHTML
-  parseInt(item)+n
+  var item = document.getElementById('app').querySelectorAll('ul.ranked-list li').innerHTML
+  
 }
